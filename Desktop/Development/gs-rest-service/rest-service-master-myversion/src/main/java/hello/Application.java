fd7cbe8ff5b601554794ce5ea8e813ae4148053c
jamesdag <dellaguardia.james@gmail.com>
2016-12-01 10:27:54 -0500

    first commit for Application.java

---
+++ Desktop/Development/gs-rest-service/rest-service-master-myversion/src/main/java/hello/Application.java
@@ -0,0 +1,13 @@
+package hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by n0223109 on 11/21/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String args[]){
+        SpringApplication.run(Application.class, args);
+    }
+}
